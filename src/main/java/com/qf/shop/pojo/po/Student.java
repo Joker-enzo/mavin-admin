ed19611525e0522ab262ea45c6047bf9b2971c56
Joker <752991742@qq.com>
2018-08-23 22:33:31 +0800

    更新实体类

---
+++ src/main/java/com/qf/shop/pojo/po/Student.java
@@ -0,0 +1,25 @@
+package com.qf.shop.pojo.po;
+
+/**
+ * Created by 李杰 on 2018/8/23.
+ */
+public class Student {
+    private int id;
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
